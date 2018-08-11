baf332261df9f294a77a21f94315bc500420975a
LEAFERx <leaferx@outlook.com>
2018-08-11 11:48:00 +0800

    update

--- app/router.js
+++ app/router.js
@@ -7,20 +7,20 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
-  router.get('/api/posts', controller.posts);
-  router.get('/api/posts/:url', controller.posts);
-  router.del('/api/posts/:url', controller.posts);
-  router.get('/api/posts/:url/comments', controller.posts);
-  router.del('/api/posts/:url/comments', controller.posts);
+  // router.get('/api/posts', controller.posts);
+  // router.get('/api/posts/:url', controller.posts);
+  // router.del('/api/posts/:url', controller.posts);
+  // router.get('/api/posts/:url/comments', controller.posts);
+  // router.del('/api/posts/:url/comments', controller.posts);
 
-  router.get('/api/comments/:id', controller.comment.get);
-  router.post('/api/comments/', controller.comment.create);
-  router.put('/api/comments/:id', controller.comment.update);
-  router.del('/api/comments/:id', controller.comment.del);
+  router.get('/api/comments/:id', controller.comments.get);
+  router.post('/api/comments', controller.comments.create);
+  router.put('/api/comments/:id', controller.comments.update);
+  router.del('/api/comments/:id', controller.comments.del);
 
-  router.get('/api/users', controller.users);
-  router.get('/api/users/:email', controller.users);
-  router.post('/api/users', controller.users.register);
+  // router.get('/api/users', controller.users);
+  // router.get('/api/users/:email', controller.users);
+  // router.post('/api/users', controller.users.register);
 
   router.post('/api/user/login', app.passport.authenticate('local'));
   router.get('/api/user/logout', controller.user.logout);
